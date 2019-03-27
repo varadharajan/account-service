1a4a02a832693403d18f54607836713d90048c86
Varadharajan Mukundan <srinathsmn@gmail.com>
2019-03-27 10:16:50 +0530

    Fix build

--- build_scripts/docker_push.sh
+++ build_scripts/docker_push.sh
@@ -3,5 +3,5 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push varadharajan/account-service:$CI_COMMIT_SHA
+docker push varadharajan/account-service:$TRAVIS_BUILD_NUMBER
 docker push varadharajan/account-service:latest
